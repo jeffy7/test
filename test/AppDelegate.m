6a88c0ab37f50102ad0ad3f42143ea2c4f8ddfa4
je_ffy <jzxiaobao@gmail.com>
2016-08-25 10:01:04 +0800

    no message

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -14,6 +14,7 @@
 
 @implementation AppDelegate
 //fix many bug
+//fix bug done
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
