5c4d7394b7da16cbc2fdaf3e8113e79d8ec4ec35
je_ffy <jzxiaobao@gmail.com>
2017-05-10 18:18:11 +0800

    未完

--- test/Sections/MineView/FollowStock/JFFollowStockViewController.m
+++ test/Sections/MineView/FollowStock/JFFollowStockViewController.m
@@ -16,6 +16,7 @@ NSString * const identifire = @"stockCell";
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UIView *tableViewHeader;
 
+@property (nonatomic, strong) UIScrollView *headerScrollview;
 
 @end
 
@@ -67,6 +68,79 @@ NSString * const identifire = @"stockCell";
     
     return cell;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    
+    return 40;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 40)];
+    headerView.backgroundColor = [UIColor whiteColor];
+    
+    CGFloat lblW = self.view.bounds.size.width / 5;
+    
+    UILabel *titleLbl = [UILabel new];
+    titleLbl.frame = CGRectMake(0, 0, lblW, 40);
+    titleLbl.text = @"股票";
+    titleLbl.backgroundColor = [UIColor colorWithWhite:0.2 alpha:0.2];
+    titleLbl.font = [UIFont systemFontOfSize:12];
+    titleLbl.textAlignment = NSTextAlignmentCenter;
+    [headerView addSubview:titleLbl];
+    
+    self.headerScrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(lblW, 0, self.view.bounds.size.width-lblW, 40)];
+    self.headerScrollview.showsVerticalScrollIndicator = NO;
+    self.headerScrollview.showsHorizontalScrollIndicator = NO;
+    self.headerScrollview.backgroundColor = [UIColor colorWithWhite:0.2 alpha:0.2];
+    
+    NSArray *topArr = @[@"估算净值", @"估算涨幅", @"最新净值", @"日涨幅", @"操作"];
+    CGFloat labelW = self.view.bounds.size.width / 5;
+    [topArr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UILabel *label = [UILabel new];
+        label.frame = CGRectMake(labelW * idx, 0, labelW, 40);
+        label.text = topArr[idx];
+        label.font = [UIFont systemFontOfSize:12];
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = NSTextAlignmentCenter;
+        [self.headerScrollview addSubview:label];
+    }];
+    self.headerScrollview.contentSize = CGSizeMake(lblW * topArr.count, 0);
+    self.headerScrollview.delegate = self;
+    
+    [headerView addSubview:self.headerScrollview];
+    
+    return headerView;
+
+}
+
+#pragma mark -
+#pragma mark - UIScrollViewDelegate
+#pragma mark-- UIScrollViewDelegate
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+//    
+//    if ([scrollView isEqual:self.headerScrollview]) {
+//        CGPoint offSet = _aroundCell.rightScrollView.contentOffset;
+//        offSet.x = scrollView.contentOffset.x;
+//        _aroundCell.rightScrollView.contentOffset = offSet;
+//    }
+//    if ([scrollView isEqual:self.tableView]) {
+//        // 发送通知
+//        [[NSNotificationCenter defaultCenter] postNotificationName:tapCellScrollNotification object:self userInfo:@{@"cellOffX":@(self.cellLastX)}];
+//    }
+//    
+//}
+//
+//-(void)scrollMove:(NSNotification*)notification{
+//    NSDictionary *noticeInfo = notification.userInfo;
+//    NSObject *obj = notification.object;
+//    float x = [noticeInfo[@"cellOffX"] floatValue];
+//    self.cellLastX = x;
+//    CGPoint offSet = self.topScrollView.contentOffset;
+//    offSet.x = x;
+//    self.topScrollView.contentOffset = offSet;
+//    obj = nil;
+//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
